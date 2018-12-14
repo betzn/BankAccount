5d61b32e8677f7c3cc4b377b1d6376c5d9fa4fa7
betzn <40780067+betzn@users.noreply.github.com>
2018-12-14 01:33:49 -0800

    Update BankAccount.java

--- BankAccount.java
+++ BankAccount.java
@@ -1,1 +1,31 @@
 
+public class BankAccount {
+String name;
+double balance;
+double transactionFee = 0.00;
+
+public boolean deposit(double amount) {
+if(amount > 0) {
+   balance = balance + amount;
+   return true;
+    }
+return false;
+  }
+
+public boolean withdraw(double amount) {
+   double temp_bal;
+if(amount > 0){
+   temp_bal = balance - amount - transactionFee;
+   if(temp_bal > 0){
+       balance = temp_bal;
+         return true;
+      }
+   return false;
+    }
+return false;
+  }
+
+public void tostring(){
+   System.out.println(name + ", $" + balance);
+  }
+}
